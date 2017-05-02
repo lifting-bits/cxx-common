eca7a8cf16612346b53e6e7627ac635bc0881274
AkshayK <iit.akshay@gmail.com>
2017-05-02 11:14:32 -0400

    cmake variables to find the drdecode libraries and include path;

---
+++ cmake/Finddrdecode.cmake
@@ -0,0 +1,13 @@
+set(LIBRARY_ROOT "${LIBRARY_REPOSITORY_ROOT}/drdecode")
+
+set(DRDECODE_FOUND TRUE)
+set(DRDECODE_INCLUDE_DIRS "${LIBRARY_ROOT}/include")
+
+set(DRDECODE_LIBRARIES
+    ${LIBRARY_ROOT}/lib64/release/libdrdecode.a
+)
+
+mark_as_advanced(FORCE DRDECODE_FOUND)
+mark_as_advanced(FORCE DRDECODE_INCLUDE_DIRS)
+mark_as_advanced(FORCE DRDECODE_LIBRARIES)
+
