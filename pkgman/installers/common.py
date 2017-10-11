a8e9fe07cf7bc4a2497c299901e0293d7822fc83
Alessandro Gario <alessandro.gario@gmail.com>
2017-10-11 22:31:24 +0200

    New target: Capstone

--- pkgman/installers/common.py
+++ pkgman/installers/common.py
@@ -1,6 +1,74 @@
 import shutil
 from utils import *
 
+def common_installer_capstone(properties):
+  repository_path = properties["repository_path"]
+  url = "https://codeload.github.com/aquynh/capstone/tar.gz/master"
+
+  capstone_tarball_path = os.path.join("sources", "capstone-master.tar.gz")
+
+  if not os.path.isfile(capstone_tarball_path):
+    tarball_path = download_file(url, "sources")
+    if tarball_path is None:
+      return False
+
+    try:
+      print(" > Renaming the tarball file...")
+      shutil.move(tarball_path, capstone_tarball_path)
+
+    except:
+      print(" > Failed to rename the tarball file")
+      return False
+
+  else:
+    print(" > Capstone already downloaded")
+
+  source_folder = os.path.realpath(os.path.join("sources", "capstone"))
+  if not os.path.isdir(source_folder):
+    if not extract_archive(capstone_tarball_path, "sources"):
+      return False
+
+    try:
+      shutil.move(os.path.join("sources", "capstone-master"), os.path.join("sources", "capstone"))
+    except:
+      print(" x Failed to rename the Capstone folder")
+      return False
+
+  else:
+    print(" > The source folder for Capstone already exists")
+
+  build_folder = os.path.join("build", "capstone")
+  if not os.path.isdir(build_folder):
+    try:
+      os.mkdir(build_folder)
+
+    except:
+      print(" x Failed to create the build folder")
+      return False
+
+  cmake_command = ["cmake",
+                   "-DCMAKE_EXE_LINKER_FLAGS=-g",
+                   "-DCMAKE_C_FLAGS=-g",
+                   "-DCAPSTONE_ARM_SUPPORT=1",
+                   "-DCAPSTONE_ARM64_SUPPORT=1",
+                   "-DCAPSTONE_BUILD_SHARED=OFF",
+                   "-DCAPSTONE_BUILD_TESTS=OFF",
+                   "-DCMAKE_INSTALL_PREFIX=" + os.path.join(repository_path, "capstone"),
+                   source_folder]
+
+  if not run_program("Configuring...", cmake_command, build_folder):
+    return False
+
+  cmake_command = ["cmake", "--build", "."]
+  if not run_program("Building...", cmake_command, build_folder):
+    return False
+
+  cmake_command = ["cmake", "--build", ".", "--target", "install"]
+  if not run_program("Installing...", cmake_command, build_folder):
+    return False
+
+  return True
+
 def common_installer_xed(properties):
   repository_path = properties["repository_path"]
 
@@ -73,4 +141,4 @@ def common_installer_xed(properties):
   if not run_program("Building and installing...", ["python", "mfile.py", "--prefix=" + os.path.join(repository_path, "xed")], xed_folder):
     return False
 
-  return False
+  return True
