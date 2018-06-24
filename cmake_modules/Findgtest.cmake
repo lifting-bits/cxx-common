6099f902195949024d790a9d5e6b47903971df97
Peter Goodman <peter.goodman@gmail.com>
2018-06-24 14:07:25 -0400

    Fix for finding gtest in a debug build.

--- cmake_modules/Findgtest.cmake
+++ cmake_modules/Findgtest.cmake
@@ -3,9 +3,16 @@ set(LIBRARY_ROOT "${LIBRARY_REPOSITORY_ROOT}/googletest")
 set(gtest_FOUND TRUE)
 set(gtest_INCLUDE_DIRS "${LIBRARY_ROOT}/include")
 
-set(gtest_LIBRARIES
-    ${LIBRARY_ROOT}/lib/libgtest.a
-)
+
+if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+  set(gtest_LIBRARIES
+      ${LIBRARY_ROOT}/lib/libgtestd.a
+  )
+else()
+  set(gtest_LIBRARIES
+      ${LIBRARY_ROOT}/lib/libgtest.a
+  )
+endif()
 
 mark_as_advanced(FORCE gtest_FOUND)
 mark_as_advanced(FORCE gtest_INCLUDE_DIRS)
