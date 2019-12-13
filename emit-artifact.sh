0b23678856a85c20b3d7f79e9766040507a590ed
Artem Dinaburg <artem@trailofbits.com>
2019-12-13 15:01:04 -0500

    Some output from emit-artifact to sanity check what its doing

--- emit-artifact.sh
+++ emit-artifact.sh
@@ -4,4 +4,5 @@
 apt-get update
 apt-get install -yqq pixz
 # compress /opt/trailofbits/libraries and emit it to $1
+echo "Compressing libraries to: ${1}"
 tar -Ipixz -cf "${1}" -C /opt/trailofbits libraries
