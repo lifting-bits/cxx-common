021c2169b791d107437d5edbbfc604e760f45379
Eric Kilmer <eric.d.kilmer@gmail.com>
2021-02-17 16:47:14 -0500

    Fixes for building on Windows 10 Static MD x64 (#677)
    * Fixes for building on Windows 10 Static MD x64
    
    * New triplet for static libraries and dynamically linked CRT (hard-coded /MD compilation option compatibility)
    
    * Slight fixes to LLVM patches for finding Z3, which doesn't seem to work the same way as Linux.
    
    * Fixes to XED compilation for Static library and dynamically linked C Runtime

---
+++ triplets/x64-windows-static-md.cmake
@@ -0,0 +1,11 @@
+# Modified from upstream's triplet of a similar name to only build Release
+# build-types
+
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+# Keep commented since a blank VCPKG_CMAKE_SYSTEM_NAME is default Windows but 
+# setting explicitly to "Windows" is wrong.
+# https://github.com/microsoft/vcpkg/blob/3f7b6777560cc9006aa43b3c4587e4d95bac7c40/scripts/cmake/vcpkg_common_definitions.cmake#L30
+# set(VCPKG_CMAKE_SYSTEM_NAME Windows)
