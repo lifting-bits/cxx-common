b2c2847da04eda5b4b77dbe3b754800e4f33f98e
Eric Kilmer <eric.d.kilmer@gmail.com>
2021-05-26 11:16:59 -0400

    Fix repo names from trailofbits to lifting-bits and remove old cxx-common

--- old/emit-artifact.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# install pixz for parallel xz
-apt-get update
-apt-get install -yqq pixz
-# compress /opt/trailofbits/libraries and emit it to $1
-echo "Compressing libraries to: ${1}"
-tar -Ipixz -cf "${1}" -C /opt/trailofbits libraries
