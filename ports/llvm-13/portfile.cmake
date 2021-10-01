ff3e624fd712649732568d3995540ee91da77893
Eric Kilmer <eric.d.kilmer@gmail.com>
2021-10-01 10:38:57 -0400

    LLVM 13.0.0

--- ports/llvm-13/portfile.cmake
+++ ports/llvm-13/portfile.cmake
@@ -1,4 +1,4 @@
-set(LLVM_VERSION "13.0.0-rc3")
+set(LLVM_VERSION "13.0.0")
 
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
@@ -6,7 +6,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO llvm/llvm-project
     REF llvmorg-${LLVM_VERSION}
-    SHA512 1401d5a4d6bb5c930d74b9cfbc8e792872f721aab7d7f0c819e2ba5cf47fb818d160c1f71784fba69827f3c9b7414aa91a585d2b813c1851b4799f9d62cebc46
+    SHA512 8004c05d32b9720fb3391783621690c1df9bd1e97e72cbff9192ed88a84b0acd303b61432145fa917b5b5e548c8cee29b24ef8547dcc8677adf4816e7a8a0eb2
     HEAD_REF main
     PATCHES
         0001-fix-install-paths.patch
