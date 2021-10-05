02b86854658d5486bf86f436a786928dcd7f41dc
Eric Kilmer <eric.d.kilmer@gmail.com>
2021-10-05 10:41:33 -0400

    Upgrade vcpkg and adjust asan triplet (#803)
    Now uses latest vcpkg commit. This requires a rebuild of the world.
    
    Merged some things into upstream vcpkg like nugettimeout so now we can use microsoft repo. Hopefully will be more stable for windows CI runs now that upstream downloads binaries of vcpkg-tool.
    
    ASAN triplet changes to be more minimal set of compiler flags. Previously, the triplet prevented some ports from building correctly.

--- triplets/x64-osx-asan.cmake
+++ triplets/x64-osx-asan.cmake
@@ -5,12 +5,13 @@ set(VCPKG_LIBRARY_LINKAGE static)
 # ASAN
 # Make sure this value matches up with https://llvm.org/docs/CMake.html "LLVM_USE_SANITIZER"
 set(VCPKG_USE_SANITIZER "Address")
+
 # If the following flags cause errors during build, you might need to manually
 # ignore the PORT and check VCPKG_USE_SANITIZER
 if(NOT PORT MATCHES "(llvm-*)")
-  set(VCPKG_CXX_FLAGS "-fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls -ffunction-sections -fdata-sections -Wl,-undefined,dynamic_lookup")
-  set(VCPKG_C_FLAGS "-fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls -ffunction-sections -fdata-sections -Wl,-undefined,dynamic_lookup")
-  set(VCPKG_LINKER_FLAGS "-fsanitize=address -Wl,-undefined,dynamic_lookup")
+  set(VCPKG_CXX_FLAGS "-fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls -ffunction-sections -fdata-sections")
+  set(VCPKG_C_FLAGS "-fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls -ffunction-sections -fdata-sections")
+  set(VCPKG_LINKER_FLAGS "-fsanitize=address")
 endif()
 
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
