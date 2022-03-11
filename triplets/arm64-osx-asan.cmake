442beb8bdf394764418d4dc240d5d46de668ac64
2over12 <ian.smith@trailofbits.com>
2022-03-11 13:24:47 -0500

    add relocation patch and fix asan triplet

--- triplets/arm64-osx-asan.cmake
+++ triplets/arm64-osx-asan.cmake
@@ -2,6 +2,8 @@ set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
+
+set(VCPKG_LINKER_FLAGS "-fsanitize=address")
 # ASAN
 # Make sure this value matches up with https://llvm.org/docs/CMake.html "LLVM_USE_SANITIZER"
 set(VCPKG_USE_SANITIZER "Address")
