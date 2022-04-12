d3df3a880b85d6fda2d0993395f17474c71d31c5
Eric Kilmer <eric.d.kilmer@gmail.com>
2022-04-12 15:11:51 -0400

    Use toolchain for asan flag overriding (#922)
    This is more correct. We have total control over the flags used when building with ASAN

--- triplets/x64-osx-rel-asan.cmake
+++ triplets/x64-osx-rel-asan.cmake
@@ -1,6 +1,8 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
+
+# Only release builds
 set(VCPKG_BUILD_TYPE release)
 
 # ASAN
@@ -10,12 +12,15 @@ set(VCPKG_USE_SANITIZER "Address")
 # If the following flags cause errors during build, you might need to manually
 # ignore the PORT and check VCPKG_USE_SANITIZER
 if(NOT PORT MATCHES "^((upb))$")
-  set(VCPKG_CXX_FLAGS "-fsanitize=address -g -fno-omit-frame-pointer -fno-optimize-sibling-calls")
-  set(VCPKG_C_FLAGS "-fsanitize=address -g -fno-omit-frame-pointer -fno-optimize-sibling-calls")
+  set(VCPKG_CXX_FLAGS "-fsanitize=address")
+  set(VCPKG_C_FLAGS "-fsanitize=address")
 endif()
 
 # Always apply sanitizer to linker flags
 set(VCPKG_LINKER_FLAGS "-fsanitize=address")
+
+# This is where we override default CMake compiler/linker flags
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../toolchain/vcpkg_unix_sanitizer_toolchain.cmake")
 
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES x86_64)
