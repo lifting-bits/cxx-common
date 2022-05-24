9e3add98836a936a90596e8ced55f2e7f44c71e5
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-05-24 15:34:35 +0200

    ports: Remove empty lib folder from gap port.

--- ports/gap/portfile.cmake
+++ ports/gap/portfile.cmake
@@ -24,6 +24,9 @@ vcpkg_cmake_config_fixup(
 file( REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" )
 file( REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share" )
 
+# we do not populate lib folder yet
+file( REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib" )
+
 file(
   INSTALL "${SOURCE_PATH}/LICENSE"
   DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
