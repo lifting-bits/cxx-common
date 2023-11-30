bf2255c90b8284b9b3d9a8fc229cb4229aa4c319
2over12 <ian.smith@trailofbits.com>
2023-11-30 17:05:37 -0500

    Custom llvm for lifting bits (#1038)

--- ports/llvm-17/portfile.cmake
+++ ports/llvm-17/portfile.cmake
@@ -1,19 +1,37 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO llvm/llvm-project
-    REF llvmorg-${VERSION}
-    SHA512 df68879cb3f23489e19bbec4aac1898d213e837132072f8bbc1a49eb561c8cc7ccdb6ae9202b68b0915c84c8f2b41e536ab690697eb8ab8c9f44d5ae600b575b
-    HEAD_REF release/17.x
-    PATCHES
-        0001-Fix-install-paths.patch
-        0006-Fix-libffi.patch
-        0020-fix-FindZ3.cmake.patch
-        0021-fix-find_dependency.patch
-        0026-fix-prefix-path-calc.patch
-        0029-Do-not-attempt-macro-expansion-on-invalid-sourceloc.patch
-)
+
+if("liftingbits-llvm" IN_LIST FEATURES)
+    vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO trail-of-forks/llvm-project
+        REF b779442fffcf46f2dfc104217e1413d7d5b538d6
+        SHA512 700ddc8c4b39b260a2041385128ef2bbe3bf6c0ff45d933d3d50d15f737892e2c7e817a4a11cda606b91364b99b10c884b76e040b321696effac7ce172720552
+        HEAD_REF main
+        PATCHES
+            0001-Fix-install-paths.patch
+            0006-Fix-libffi.patch
+            0020-fix-FindZ3.cmake.patch
+            0021-fix-find_dependency.patch
+            0026-fix-prefix-path-calc.patch
+            0029-Do-not-attempt-macro-expansion-on-invalid-sourceloc.patch
+    )
+else()
+      vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO llvm/llvm-project
+        REF llvmorg-${VERSION}
+        SHA512 df68879cb3f23489e19bbec4aac1898d213e837132072f8bbc1a49eb561c8cc7ccdb6ae9202b68b0915c84c8f2b41e536ab690697eb8ab8c9f44d5ae600b575b
+        HEAD_REF release/17.x
+        PATCHES
+            0001-Fix-install-paths.patch
+            0006-Fix-libffi.patch
+            0020-fix-FindZ3.cmake.patch
+            0021-fix-find_dependency.patch
+            0026-fix-prefix-path-calc.patch
+            0029-Do-not-attempt-macro-expansion-on-invalid-sourceloc.patch
+    )  
+endif()
 
 string(REPLACE "." ";" VERSION_LIST ${VERSION})
 list(GET VERSION_LIST 0 LLVM_VERSION_MAJOR)
