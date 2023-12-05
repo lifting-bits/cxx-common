e26dc98ced8455a427e2290db15672dbb1215207
2over12 <ian.smith@trailofbits.com>
2023-12-05 08:24:57 -0500

    bump hash

--- ports/llvm-17/portfile.cmake
+++ ports/llvm-17/portfile.cmake
@@ -6,7 +6,7 @@ if("liftingbits-llvm" IN_LIST FEATURES)
         OUT_SOURCE_PATH SOURCE_PATH
         REPO trail-of-forks/llvm-project
         REF bc8b628fb02377df460e1002c986d640f158fea3
-        SHA512 700ddc8c4b39b260a2041385128ef2bbe3bf6c0ff45d933d3d50d15f737892e2c7e817a4a11cda606b91364b99b10c884b76e040b321696effac7ce172720552
+        SHA512 342ac89602c36d1c339994f35ad90f196528de1301f9ad27465a5fddd4f65e2675e3bc8d9f5840ecd8bb6f1636a62ceadac2b255b50343538e81feac027b98e7
         HEAD_REF main
         PATCHES
             0001-Fix-install-paths.patch
