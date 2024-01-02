7f3a955d2645793d32e1448cb63cd5d26dac1689
Eric Kilmer <eric.d.kilmer@gmail.com>
2024-01-02 18:48:15 -0500

    Simplify

--- ports/re2c/portfile.cmake
+++ ports/re2c/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO skvadrik/re2c
-    REF 3.1
+    REF "${VERSION}"
     SHA512 7528d78e1354c774783e63e05553b7a772f8185b43b988cddf79a527ed63316f18e6f9fb3a63ae4d5c83c9f4de2b672b0e61898d96bdd6f15a1eaa7b4d47c757
     HEAD_REF master
 )
