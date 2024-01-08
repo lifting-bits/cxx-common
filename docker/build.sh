732c3364b444dc8b0d5847355437cb0c7487895a
Eric Kilmer <eric.d.kilmer@gmail.com>
2024-01-08 11:37:27 -0500

    Test with multiple Ubuntu versions

--- docker/build.sh
+++ docker/build.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 # build vcpkg libraries as well as all lifting-bits tools
 
 # Ubuntu versions to build
-UBUNTU_VERSION_MATRIX=( "22.04" )
+UBUNTU_VERSION_MATRIX=( "22.04" "24.04" )
 
 for version in "${UBUNTU_VERSION_MATRIX[@]}"; do
   # Always pull from upstream
@@ -14,13 +14,13 @@ for version in "${UBUNTU_VERSION_MATRIX[@]}"; do
 
   # Also remember to change the '.github/workflows/vcpkg_docker.yml' variable
   # Image identification
-  tag="vcpkg-builder-ubuntu-${version}-v2"
+  image="vcpkg-builder-ubuntu-${version}"
 
   # Build
   docker build \
       -f Dockerfile.ubuntu.vcpkg \
       --no-cache \
-      --build-arg "DISTRO_VERSION=${version}" \
-      -t "${tag}" \
+      --build-arg "UBUNTU_VERSION=${version}" \
+      -t "${image}" \
       .
 done
