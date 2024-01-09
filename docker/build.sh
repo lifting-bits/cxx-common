4a055015f03510753dec448a3982a6623189ef7a
Eric Kilmer <eric.d.kilmer@gmail.com>
2024-01-09 16:16:50 -0500

    Add back "-v2" to image name
    Now that we use the original image naming scheme, this needs to be added
    back

--- docker/build.sh
+++ docker/build.sh
@@ -8,19 +8,20 @@ set -euo pipefail
 # Ubuntu versions to build
 UBUNTU_VERSION_MATRIX=( "22.04" "24.04" )
 
-for version in "${UBUNTU_VERSION_MATRIX[@]}"; do
+for ubuntu_version in "${UBUNTU_VERSION_MATRIX[@]}"; do
   # Always pull from upstream
-  docker pull "ubuntu:${version}"
+  docker pull "ubuntu:${ubuntu_version}"
 
+  # Image identification. "v2" Image version is to identify big changes to the
+  # build toolchain like LLVM version
   # Also remember to change the '.github/workflows/vcpkg_docker.yml' variable
-  # Image identification
-  image=vcpkg-builder-ubuntu
+  image="vcpkg-builder-ubuntu-v2"
 
   # Build
   docker build \
       -f Dockerfile.ubuntu.vcpkg \
       --no-cache \
-      --build-arg "UBUNTU_VERSION=${version}" \
-      -t "${image}:${version}" \
+      --build-arg "UBUNTU_VERSION=${ubuntu_version}" \
+      -t "${image}:${ubuntu_version}" \
       .
 done
